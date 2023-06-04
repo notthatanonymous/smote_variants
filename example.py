16edb818a5780effdbd2b068995a941b351402c5
Musfiqur Rahman <musfiqurrahman.lab08@gmail.com>
2023-06-04 15:50:12 -0400

    updating example file

--- example.py
+++ example.py
@@ -1,24 +1,36 @@
+import smote_variants as sv
+import imblearn.datasets as imb_datasets
+
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import (cross_validate, StratifiedKFold)
-from sklearn.datasets import load_iris
+from sklearn.neighbors import KNeighborsClassifier
 
-import smote_variants as sv
+libras= imb_datasets.fetch_datasets()['libras_move']
+X, y= libras['data'], libras['target']
 
-dataset = load_iris()
-X = dataset['data']
-y = dataset['target']
-
-y[y > 1] = 1
-
-oversampler = ('smote_variants', 'ADASYN', {'proportion': 1.0})
+oversampler = ('smote_variants', 'MulticlassOversampling',
+                {'oversampler': 'distance_SMOTE', 'oversampler_params': {}})
 
 classifier = ('sklearn.neighbors', 'KNeighborsClassifier', {})
 
+# Constructing a pipeline with oversampling and classification as the last step
 model= Pipeline([('scale', StandardScaler()),
-('clf', sv.classifiers.OversamplingClassifier(oversampler, classifier))])
-scoring = ['accuracy','precision', 'recall','f1','roc_auc']
-scores = cross_validate(model,X,y,cv=StratifiedKFold(n_splits=10),scoring='roc_auc',error_score='raise')
+                ('clf', sv.classifiers.OversamplingClassifier(oversampler, classifier))])
 
+param_grid= {'clf__oversampler':[('smote_variants', 'distance_SMOTE', {'proportion': 0.5}),
+                                ('smote_variants', 'distance_SMOTE', {'proportion': 1.0}),
+                                ('smote_variants', 'distance_SMOTE', {'proportion': 1.5})]}
 
-print(scores)
+# Specifying the gridsearch for model selection
+grid= GridSearchCV(model,
+                  param_grid=param_grid,
+                  cv=3,
+                  n_jobs=1,
+                  verbose=2,
+                  scoring='accuracy')
+
+# Fitting the pipeline
+grid.fit(X, y)
+
+print(grid.best_params_, grid.best_score_)
